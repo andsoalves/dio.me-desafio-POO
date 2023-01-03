04db0f6e1c87c84f68a9de994b070c986241c70d
Anderson Alves <alves_anderson@outlook.com>
2023-01-03 20:14:51 -0300

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setDescricao("Java");
+        curso1.setTitulo("java");
+        curso1.setCargaHoraria(80);
+
+        System.out.println(curso1);
     }
 }
